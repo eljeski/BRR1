742fcf53d2fa54dc1208cfd21a2a2c275356582b
Jorge Esquivel <jbesquivel64@hotmail.com>
2022-08-11 13:23:32 -0600

    Blog con iteracion

---
+++ db/migrate/20220811191801_add_userid_comments.rb
@@ -0,0 +1,4 @@
+class AddUseridComments < ActiveRecord::Migration[7.0]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_11_153924) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_11_191801) do
   create_table "comments", force: :cascade do |t|
     t.integer "post_id", null: false
     t.text "body"
@@ -22,6 +22,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_11_153924) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
